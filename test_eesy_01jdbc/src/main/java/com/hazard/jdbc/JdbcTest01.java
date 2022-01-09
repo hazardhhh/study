7ad8b3a93a995d7925309fa8f7403df89e6f39db
hazard <1145755268@qq.com>
2022-01-09 22:37:01 +0800

    测试

--- test_eesy_01jdbc/src/main/java/com/hazard/jdbc/JdbcTest01.java
+++ test_eesy_01jdbc/src/main/java/com/hazard/jdbc/JdbcTest01.java
@@ -8,9 +8,10 @@ import java.sql.ResultSet;
 public class JdbcTest01 {
     public static void main(String[] args) throws Exception{
         //1.注册驱动
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Class.forName("com.mysql.jdbc.Driver");
         //2.获取连接
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eesy", "root", "111111");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eesy?serverTimezone=GMT&useSSL=false", "root", "111111");
         //3.获取操作数据库的预处理对象
         PreparedStatement pstm = conn.prepareStatement("select * from account");
         //4.执行sql,得到结果集
